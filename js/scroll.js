bf5402771f369d6909a97e0177b62dbb7f14a96e
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-02-06 23:45:02 +0900

    スクロール位置のアクション

---
+++ js/scroll.js
@@ -0,0 +1,26 @@
+function ScrollNavi() {
+  var winScrollTop = $(this).scrollTop();
+  var pt1 = $("#scroll-pt1").offset().top - 150;
+  var pt2 = $("#scroll-pt2").offset().top - 50;
+  var pt3 = $("#scroll-pt3").offset().top - 150;
+  var pt4 = $("#scroll-pt4").offset().top - 150;
+
+  if (winScrollTop >= pt1) {
+    $(".scroll-nav").addClass("fadeIn");
+  } else if (winScrollTop >= pt2) {
+    $(".scroll-nav").css("right", "20px");
+  }
+}
+
+$(window).scroll(function () {
+  ScrollNavi();
+});
+
+$(window).on('load', function () {
+  ScrollNavi();
+});
+
+
+
+
+
