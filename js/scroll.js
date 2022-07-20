5c032da23c666c9acffd1f67c65bbe1cb3ab8e31
吉野弘之 <yoshinohiroyuki@yoshinohiroyuki-no-MacBook-Pro.local>
2022-07-20 15:03:11 +0900

    test

--- js/scroll.js
+++ js/scroll.js
@@ -1,47 +1,65 @@
-/* function ScrollNavi() {
-  var winScrollTop = $(this).scrollTop();
-  var pt1 = $("#scroll-pt1").offset().top - 150;
-  var pt2 = $("#scroll-pt2").offset().top - 150;
-  var pt3 = $("#scroll-pt3").offset().top - 150;
-  var pt4 = $("#scroll-pt4").offset().top - 150;
+//ページ内リンクに飛ぶaタグを取得
+const scrollTrigger = document.querySelectorAll('a[href^="#"]');
 
-  if (winScrollTop >= pt1) {
-    $(".scroll-nav").addClass("fadeIn");
-    console.log('エリア1');
-  } else if (winScrollTop >= pt1 && winScrollTop >= 2) {
-    $(".scroll-nav").removeClass("fadeIn");
-  };
-};
+for (let i = 0; i < scrollTrigger.length; i++) {
+  scrollTrigger[i].addEventListener('click', (e) => {
 
-$(window).scroll(function () {
-  ScrollNavi();
+    e.preventDefault();
+    let href = scrollTrigger[i].getAttribute('href');
+    let target = document.getElementById(href.replace('#', ''));
+    const targetPoint = target.getBoundingClientRect().top + window.pageYOffset;
+    window.scrollTo({
+      top: targetPoint,
+      behavior: 'smooth'
+    });
+  });
+}
+
+const scrollArea = document.querySelectorAll('.sec');
+scrollArea.forEach(function (area) {
+  console.log(area);
 });
 
-$(window).on('load', function () {
-  ScrollNavi();
-}); */
-
-window.addEventListener('scroll', function () {
-  let position = this.document.getElementById('scroll-pt1').getBoundingClientRect().top;
+const scrollPoint = function () {
+  let position1 = this.document.getElementById('scroll-pt1').getBoundingClientRect().top;
   let position2 = this.document.getElementById('scroll-pt2').getBoundingClientRect().top;
-  let position3 = this.document.getElementById('scroll-pt1').getBoundingClientRect().top;
-  let position4 = this.document.getElementById('scroll-pt2').getBoundingClientRect().top;
+  let position3 = this.document.getElementById('scroll-pt3').getBoundingClientRect().top;
+  let position4 = this.document.getElementById('scroll-pt4').getBoundingClientRect().top;
+  let btn1 = this.document.getElementById('btn1');
+  let btn2 = this.document.getElementById('btn2');
+  let btn3 = this.document.getElementById('btn3');
+  let btn4 = this.document.getElementById('btn4');
+  let frame1 = this.document.querySelector('.frame01');
+  let frame2 = this.document.querySelector('.frame02');
+  let frame3 = this.document.querySelector('.frame03');
+  let frame4 = this.document.querySelector('.frame04');
   let navItem = document.querySelector('.scroll-nav');
-  let navNum = navItem.length;
-  let nuvItemElm = new Array;
-  let contentItem = new Array;
-  let winHeight = window.innerHeight;
+  let winHeight = window.innerHeight / 3;
 
-  if (winHeight >= position) {
+  if (winHeight >= position1) {
     navItem.style.opacity = 1;
+    btn1.classList.add('addSquare');
+    frame1.classList.add('addFrame');
     if (winHeight >= position2) {
-      navItem.style.right = "90%";  
+      navItem.style.right = '95%';
+      btn1.classList.remove('addSquare');
+      frame1.classList.remove('addFrame');
+      btn2.classList.add('addSquare');
+      frame2.classList.add('addFrame');
     } else {
-      navItem.style.right = "10px";
+      navItem.style.right = "20px";
     }
   } else {
-    navItem.style.opacity = 0;  
+    navItem.style.opacity = 0;
   }
+};
+
+window.addEventListener('scroll', function () {
+  scrollPoint();
+});
+
+window.addEventListener('load', function () {
+  scrollPoint();
 });
 
 
